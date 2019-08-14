a7756dc6f5eb59641f2c362a1511747425a82754
Vladislav Manakov <footzi@mail.ru>
2019-08-14 21:40:46 +0300

    add reducer

--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -7,12 +7,85 @@ import { ADD_INGREDIENT } from '../actions/ingredients';
 // Обратите внимание на `orders.test.js`.
 // Он поможет понять, какие значения должен возвращать редьюсер.
 
+const getPositionNext = position => {
+  switch (position) {
+    case 'clients':
+      return 'conveyor_1';
+    case 'conveyor_1':
+      return 'conveyor_2';
+    case 'conveyor_2':
+      return 'conveyor_3';
+    case 'conveyor_3':
+      return 'conveyor_4';
+    default:
+      return position;
+  }
+};
+
+const getPositionPrev = position => {
+  switch (position) {
+    case 'conveyor_4':
+      return 'conveyor_3';
+    case 'conveyor_3':
+      return 'conveyor_2';
+    case 'conveyor_2':
+      return 'conveyor_1';
+    default:
+      return position;
+  }
+};
+
 export default (state = [], action) => {
   switch (action.type) {
+    case CREATE_NEW_ORDER:
+      return [
+        ...state,
+        {
+          id: action.payload.id,
+          recipe: action.payload.recipe,
+          ingredients: [],
+          position: 'clients'
+        }
+      ];
+    case MOVE_ORDER_NEXT:
+      return [
+        ...state.map(item => {
+          if (item.id === action.payload) {
+            item.position = getPositionNext(item.position);
+          }
+
+          if (item.position === 'conveyor_4' && item.ingredients.length === item.recipe.length) {
+            item.position = 'finish'
+          }
+          return item;
+        })
+      ];
+
+    case MOVE_ORDER_BACK:
+      return [
+        ...state.map(item => {
+          if (item.id === action.payload) {
+            item.position = getPositionPrev(item.position);
+          }
+          return item;
+        })
+      ];
+    case ADD_INGREDIENT: 
+      return [
+        ...state.map(item => {
+          const { from, ingredient} = action.payload;
+
+          if (item.position === from && item.recipe.includes(ingredient)) {
+            item.ingredients = [...item.ingredients, ingredient];
+          }
+          return item;
+        })
+      ];
     default:
       return state;
   }
 };
 
-export const getOrdersFor = (state, position) =>
-  state.orders.filter(order => order.position === position);
+export const getOrdersFor = (state, position) => {
+  return state.orders.filter(order => order.position === position);
+};
